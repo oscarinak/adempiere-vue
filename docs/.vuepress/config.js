a3ebeced54a29b1d858b3ed4c4478f1de9214d64
Keyka Silva <52334530+oscarinak@users.noreply.github.com>
2021-05-04 16:33:09 -0400

    Add document path access-to-records.md
    Add document path access-to-records.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -285,6 +285,7 @@ function genAdvancedSidebar(type = '') {
 function genComponentSidebar(type = '') {
   const mapArr = [
     '/guide/components/preference.md'
+    '/guide/components/access-to-records.md'
   ]
   return mapArr.map(i => {
     return type + i
